9ce483d22cd74475dc9f278b6e8b96335e171a94
parthbpatel <parthtech3010@gmail.com>
2019-12-19 18:13:16 +0530

    add js, add cocoon-gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :places, only: [:new, :create, :edit, :update]
 
-  root 'places#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'places#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
